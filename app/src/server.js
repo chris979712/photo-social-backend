c7c4b0394ed613a3eaf61b86b45eaa76fef194a9
chris <chrisvasquez985@gmail.com>
2025-04-26 20:59:43 -0600

    Cambio docker file

--- app/src/server.js
+++ app/src/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/',(req,res)=>{
-  res.json({message: 'Bienvenido al servidor de GameLogAPI'});
+  res.json({message: 'Bienvenido al servidor Photo-Social-Backend'});
 })
 
 app.use("/uploads", express.static("uploads")); 
